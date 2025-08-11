d49239e896986f3303018696bc3b23dde85a280b
BRUNO <belottobruno88@gmail.com>
2025-08-11 14:04:17 +0000

    <script   src="https://cdn.utmify.com.br/scripts/utms/latest.js"   data-

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
+
 import type { Metadata } from 'next';
 import { Toaster } from "@/components/ui/toaster";
 import './globals.css';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'Saboaria da Vó Rita',
@@ -18,6 +20,13 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
+        <script
+          src="https://cdn.utmify.com.br/scripts/utms/latest.js"
+          data-utmify-prevent-xcod-sck
+          data-utmify-prevent-subids
+          async
+          defer
+        ></script>
       </head>
       <body className="font-body bg-background text-foreground antialiased">
         {children}
