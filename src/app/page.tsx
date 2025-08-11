44f653fdc550ea3ad4ecc5ead09d445303600317
BRUNO <belottobruno88@gmail.com>
2025-08-11 23:10:50 +0000

    O video esta iniciando sem audio, o video deve comecar com som assim que

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,7 +45,6 @@ export default function Home() {
                   src="https://www.dropbox.com/scl/fi/u0zfvs9cmqrg57dqnqqlt/Saboaria-Up-Feito-com-o-Clipchamp.mp4?rlkey=yvq5ib2xc8xa72se5mwujej5x&st=2wg58m3n&raw=1"
                   autoPlay
                   loop
-                  muted
                   playsInline
                   controls
                   className="w-full h-full object-cover"
